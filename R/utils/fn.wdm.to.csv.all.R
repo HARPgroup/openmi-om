01eb9a2c831879b78bb8621e0408bbe50e5923dc
Daniel Hildebrand <danielh7@vt.edu>
2019-07-15 14:02:22 -0400

    ups

--- R/utils/fn.wdm.to.csv.all.R
+++ R/utils/fn.wdm.to.csv.all.R
@@ -5,8 +5,13 @@ wdm.to.csv.all <- function(wdmpath, outputdir, start.year, end.year, dsn) {
     quick.wdm.2.txt.inputs <- paste(wdm.list[i], start.year, end.year, dsn, sep = ',')
     run.quick.wdm.2.txt <- paste("echo", quick.wdm.2.txt.inputs, "| /opt/model/p6-devel/p6-4.2018/code/bin/quick_wdm_2_txt_hour_2_hour", sep = ' ')
     system(command = run.quick.wdm.2.txt)
+    if (length(dsn) == 3) {
+      dsn.name == paste0('0', dsn)
+    } else {
+      dsn.name == dsn
+    }
     csvname <- strsplit(wdm.list[i], split = ".wdm")
-    csvname <- paste(csvname[[1]], ".csv", sep = '')
+    csvname <- paste(csvname[[1]], '_', dsn.name, ".csv", sep = '')
     move.to.outputdir <- paste("mv", csvname, outputdir)
     system(command = move.to.outputdir)
   }
