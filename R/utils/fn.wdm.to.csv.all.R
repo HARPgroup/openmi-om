17091643da538580bf2677d006a7fbf06affa59c
Daniel Hildebrand <danielh7@vt.edu>
2019-07-15 13:55:05 -0400

    ups

--- R/utils/fn.wdm.to.csv.all.R
+++ R/utils/fn.wdm.to.csv.all.R
@@ -1,7 +1,7 @@
 wdm.to.csv.all <- function(wdmpath, outputdir, start.year, end.year, dsn) {
   wdm.list <- list.files(path = wdmpath, pattern = "\\.wdm$", recursive = FALSE)
   for (i in 1:length(wdm.list)) {
-    print('Generating', i, 'of', length(wdm.list, 'csv files'))
+    print('Generating', i, 'of', length(wdm.list), 'csv files')
     quick.wdm.2.txt.inputs <- paste(wdm.list[i], start.year, end.year, dsn, sep = ',')
     run.quick.wdm.2.txt <- paste("echo", quick.wdm.2.txt.inputs, "| /opt/model/p6-devel/p6-4.2018/code/bin/quick_wdm_2_txt_hour_2_hour", sep = ' ')
     system(command = run.quick.wdm.2.txt)
