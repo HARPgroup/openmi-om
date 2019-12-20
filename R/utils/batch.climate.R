f66b2922c8d5d677b7c61a4006346107b6d1b1e4
Daniel Hildebrand <danielh7@vt.edu>
2019-12-20 17:38:18 -0500

    batch climate

--- R/utils/batch.climate.R
+++ R/utils/batch.climate.R
@@ -1,6 +1,12 @@
 source('~/openmi-om/R/utils/fn.climate_evap.and.prcp.R')
 
 batch.climate <- function(outpath) {
-  wdm.list <- list.files(path = outpath, pattern = "\\_1000.csv$", recursive = FALSE)
-  names.pt1 <- strsplit(wdm.list, "met_")
+  wdm.list <- list.files(path = outpath, pattern = "_1000\\.csv$", recursive = FALSE)
+  names.pt1 <- sapply(strsplit(wdm.list, "met_"), "[[", 2)
+  segs <- sapply(strsplit(names.pt1, "_1000.csv"), "[[", 1)
+  
+  for (i in 1:length(segs)) {
+    climate_evap.and.prcp(segment = segs(i), wdmpath = '/opt/model/p6/p6_gb604', outpath = outpath)
+    print('creating .csv file', i, 'of', length(segs), sep = ' ')
+  }
 }
