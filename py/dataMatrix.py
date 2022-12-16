7f88be04f14bf21616902a462b4be409eb6bcce8
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2022-12-16 12:48:15 -0500

    working demo of iterable model from object code

--- py/dataMatrix.py
+++ py/dataMatrix.py
@@ -1,21 +1,10 @@
 class dataMatrix(modelObject):
-    def __init__(self, name, state_ix, state_paths, dict_ix):
-        super(Equation, self).__init__(name, state_ix, state_paths, dict_ix)
+    def __init__(self, name, container = False, matrix_vals = []):
+        super(dataMatrix, self).__init__(name, container)
         self.lu_type1 = ""
-        self.matrix = [] # gets passed in at creation.  Refers to path "/OBJECTS/DataMatrix/RCHRES_0001/stage_storage_discharge/matrix"
+        self.matrix = matrix_vals # gets passed in at creation.  Refers to path "/OBJECTS/DataMatrix/RCHRES_0001/stage_storage_discharge/matrix"
+        # self.op_matrix = [] # this is the final opcoded matrix for  runtime
         self.optype = 2 # 0 - shell object, 1 - equation, 2 - datamatrix, 3 - input, 4 - broadcastChannel, 5 - ?
-    
-    def ncode_step(self):
-        # return string with code that is executable in @njit function with ts and object state variables
-        code_lines = []
-        if self.lu_type1 == "":
-            # this is only sample code of what a sample lookup might look like
-            # note: three consecutive quote marks (""") tell python that this is a multi-line string
-            code_lines.append("""OBJECTS_DATAMTRIX_0001 = array([[0.000000e+00, 1.700000e+02, 0.000000e+00],
-                [1.952000e+05, 2.400000e+02, 8.890600e+03],
-                [2.042528e+05, 2.410000e+02, 9.301500e+03],
-                [2.137360e+05, 2.420000e+02, 9.712500e+03]])""")
-            code_lines.append('ts["/RESULTS/RCHRES_001/SPECL/elev"][step] = np.interp(ts["/RESULTS/RCHRES_001/SPECL/Volume"][step],OBJECTS_DATAMATRIX_0001[:, 0][0:], OBJECTS_DATAMATRIX_0001[:, 1][0:])')
     
     def tokenize(self):
         # - insure we have a entity_ix pointing to state_ix
@@ -25,3 +14,39 @@ class dataMatrix(modelObject):
         # - store array in dict_ix keyed with entity_ix
         # - get entity_ix for lookup key(s)
         # - create tokenized array with entity_ix, lookup types, 
+        # renders tokens for high speed execution
+        self.ops = [self.optype, self.ix]
+    
+    def add_op_tokens(self):
+        # this puts the tokens into the global simulation queue 
+        # can be customized by subclasses to add multiple lines if needed.
+        self.op_tokens[self.ix] = self.ops
+        self.dict_ix[self.ix] = self.op_matrix
+
+# njit functions for runtime
+
+@njit
+def specl_lookup(data_table, keyval, lutype, valcol):
+    if lutype == 2: #stair-step
+        idx = (data_table[:, 0][0:][(data_table[:, 0][0:]- keyval) <= 0]).argmax()
+        luval = data_table[:, valcol][0:][idx]
+    elif lutype == 1: # interpolate
+        luval = np.interp(keyval,data_table[:, 0][0:], data_table[:, valcol][0:])
+        
+    # show value at tis point
+    return luval
+
+@njit
+def exec_tbl_eval(op, state_ix, dict_ix):
+    ix = op[1]
+    dix = op[2]
+    mx_type = op[3] # not used yet, what type of table?  in past this was always 1-d or 2-d 
+    key1_ix = op[4]
+    #print("ix, dict_ix, mx_type, key1_ix", ix, dix, mx_type, key1_ix)
+    lutype = op[5]
+    valcol = op[8]
+    data_table = dict_ix[dix]
+    keyval = state_ix[key1_ix]
+    #print("Key, ltype, val", keyval, lutype, valcol)
+    result = specl_lookup(data_table, keyval, lutype, valcol)
+    return result
--- py/example_intake.py
+++ py/example_intake.py
@@ -1,5 +1,7 @@
-op_tokens, state_paths, state_ix, dict_ix = init_sim_dicts()
-river = modelObject('RCHRES_R001', state_ix, state_paths, dict_ix)
+op_tokens, state_paths, state_ix, dict_ix, ts_ix = init_sim_dicts()
+modelObject.op_tokens, modelObject.state_paths, modelObject.state_ix, modelObject.dict_ix = (op_tokens, state_paths, state_ix, dict_ix)
+
+river = modelObject('RCHRES_R001')
 river.state_path = specl_state_path('RCHRES', 1)
 river.register_path()
 
@@ -8,32 +10,57 @@ river.add_input("Qin", f'{river.state_path}/HYDR/IVOL')
 # river.inputs["Qin"] = ["/TIMESERIES/TS011"]
 # river.add_input("ps_mgd", "/TIMESERIES/TS3000")
 
-facility = modelObject('facility', state_ix, state_paths, dict_ix)
-facility.container = river
+facility = modelObject('facility', river)
 facility.make_state_path()
 facility.register_path()
 
-Qintake = Equation('Qintake', state_ix, state_paths, dict_ix)
-Qintake.container = facility
+Qintake = Equation('Qintake', facility, "Qin * 1.21")
 Qintake.make_state_path()
 Qintake.register_path()
+Qintake.tokenize()
 
-river.find_var_path("Qin")
-Qintake.find_var_path("Qin")
-Qintake.equation = "Qin * 1.21"
-Qintake.tokenize_ops() 
-Qintake.tokenize_vars()
+flowby = Equation('flowby', facility, "Qintake * 0.9")
+flowby.register_path()
+flowby.tokenize()
 
-flowby = Equation('flowby', state_ix, state_paths, dict_ix)
-flowby.container = facility
-flowby.equation = "Qintake * 0.9"
-flowby.tokenize_ops() 
-flowby.tokenize_vars()
+wd_mgd = Equation('demand_mgd', facility, "3.0 + 0.0")
+wd_mgd.register_path()
+wd_mgd.tokenize() 
 
-wd_mgd = Equation('demand_mgd', state_ix, state_paths, dict_ix)
-wd_mgd.container = facility
-wd_mgd.equation = "3.0"
-wd_mgd.tokenize_ops() 
-wd_mgd.tokenize_vars()
+# now connect the wd_mgd back to the river with a direct link.  
+# This is not how we'll do it for most simulations as there may be multiple inputs but will do for now
+hydr = modelObject('HYDR', river)
+hydr.register_path()
+O1 = modelLinkage('O1', hydr, wd_mgd.state_path, 2)
+O1.register_path()
+O1.tokenize() 
+
+
+river.add_op_tokens()
+facility.add_op_tokens()
+Qintake.add_op_tokens()
+flowby.add_op_tokens()
+wd_mgd.add_op_tokens()
+O1.add_op_tokens()
+
+step(op_tokens, state_ix, dict_ix, ts_ix, 1)
+
+steps=40*365*24
+start = time.time()
+num = iterate_models(op_tokens, state_ix, dict_ix, ts_ix, steps)
+end = time.time()
+print(end - start, "seconds")
+
+
+# get a list of keys for all hydr state variables
+hydr_state = ["DEP","IVOL","O1","O2","O3","OVOL1","OVOL2","OVOL3","PRSUPY","RO","ROVOL","SAREA","TAU","USTAR","VOL","VOLEV"]
+hydr_ix = Dict.empty(key_type=types.unicode_type, value_type=types.int64)
+for i in hydr_state:
+    var_path = f'{domain}/{i}'
+    hydr_ix[i] = set_state(state_ix, state_paths, var_path, 0.0)
+
+o1_ix, o2_ix, o3_ix = hydr_ix['O1'], hydr_ix['O2'], hydr_ix['O3']
+outdgt[:] = [ state_ix[o1_ix], state_ix[o2_ix], state_ix[o3_ix] ]
+
 
 
